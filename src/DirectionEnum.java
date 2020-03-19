79568d95a01d6958d5c10734d30337e5c420d052
mob-driver <info@aegisoft.be>
2020-03-19 15:24:26 +0100

    Refactoring: Added degrees to DirectionEnum to be able to compute the next direction instead of ifelsing the hell out of it

--- src/DirectionEnum.java
+++ src/DirectionEnum.java
@@ -1,6 +1,13 @@
 public enum DirectionEnum {
-	NORTH,
-	SOUTH,
-	EAST,
-	WEST
+	NORTH (0),
+	EAST(90),
+	SOUTH(180),
+	WEST (270);
+
+	private int degrees;
+
+	DirectionEnum(int degrees){
+		this.degrees = degrees;
+	}
 }
+
