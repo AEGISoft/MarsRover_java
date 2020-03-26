5df732c8e0d4898f399f9fae5634e232e9faee1e
Tim Boodts <tim.boodts@sfpd.fgov.be>
2020-03-26 15:15:22 +0100

    refactor Direction for readability

--- src/Direction.java
+++ src/Direction.java
@@ -1,64 +1,32 @@
-import java.awt.*;
-
 public enum Direction {
 	NORTH {
-		@Override Location moveBackwards() {
-			return new Location(0, - 1);
-		}
-
-		@Override Location moveForwards() {
-			return new Location(0,1);
-		}
+		@Override Location moveBackwards() { return new Location(0, - 1); }
+		@Override Location moveForwards() { return new Location(0,1); }
+		@Override Direction turnRight() { return EAST; }
+		@Override Direction turnLeft() { return WEST; }
 	},
 	EAST{
-		@Override Location moveBackwards() {
-			return new Location(-1,0);
-		}
-
-		@Override Location moveForwards() {
-			return new Location(1, 0);
-		}
+		@Override Location moveBackwards() { return new Location(-1,0); }
+		@Override Location moveForwards() { return new Location(1, 0); }
+		@Override Direction turnRight() { return SOUTH; }
+		@Override Direction turnLeft() { return NORTH; }
 	},
 	SOUTH{
-		@Override Location moveBackwards() {
-			return new Location(0, 1);
-		}
-
-		@Override Location moveForwards() {
-			return new Location(0, -1);
-		}
+		@Override Location moveBackwards() { return new Location(0, 1); }
+		@Override Location moveForwards() { return new Location(0, -1); }
+		@Override Direction turnRight() { return WEST; }
+		@Override Direction turnLeft() { return EAST; }
 	},
 	WEST{
-		@Override Location moveBackwards() {
-			return new Location(1,0);
-		}
-
-		@Override Location moveForwards() {
-			return new Location(- 1, 0);
-		}
+		@Override Location moveBackwards() { return new Location(1,0); }
+		@Override Location moveForwards() { return new Location(- 1, 0); }
+		@Override Direction turnRight() { return NORTH; }
+		@Override Direction turnLeft() { return SOUTH; }
 	};
 
 	abstract Location moveBackwards();
-
 	abstract Location moveForwards();
-
-	Direction turnRight(){
-		switch (this){
-			case NORTH: return EAST;
-			case EAST: return SOUTH;
-			case SOUTH: return WEST;
-			case WEST: return NORTH;
-			default: throw new IllegalArgumentException("unsupported direction");
-		}
-	}
-	Direction turnLeft(){
-		switch (this){
-			case NORTH: return WEST;
-			case WEST: return SOUTH;
-			case SOUTH: return EAST;
-			case EAST: return NORTH;
-			default: throw new IllegalArgumentException("unsupported direction");
-		}
-	}
+	abstract Direction turnRight();
+	abstract Direction turnLeft();
 }
 
