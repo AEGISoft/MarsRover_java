a504cb03ffa7c3901806c0b3ff66cdcbc6429ce1
Tim Boodts <tim.boodts@sfpd.fgov.be>
2020-04-06 11:19:31 +0200

    Refactoring: reduce amount of circular dependencies

--- src/Direction.java
+++ src/Direction.java
@@ -1,25 +1,25 @@
 public enum Direction {
 	NORTH {
-		@Override Location moveBackwards() { return new Location(0, - 1); }
-		@Override Location moveForwards() { return new Location(0,1); }
+		@Override Location moveBackwards() { return Step.SOUTH; }
+		@Override Location moveForwards() { return Step.NORTH; }
 		@Override Direction turnRight() { return EAST; }
 		@Override Direction turnLeft() { return WEST; }
 	},
 	EAST{
-		@Override Location moveBackwards() { return new Location(-1,0); }
-		@Override Location moveForwards() { return new Location(1, 0); }
+		@Override Location moveBackwards() { return Step.WEST; }
+		@Override Location moveForwards() { return Step.EAST; }
 		@Override Direction turnRight() { return SOUTH; }
 		@Override Direction turnLeft() { return NORTH; }
 	},
 	SOUTH{
-		@Override Location moveBackwards() { return new Location(0, 1); }
-		@Override Location moveForwards() { return new Location(0, -1); }
+		@Override Location moveBackwards() { return Step.NORTH; }
+		@Override Location moveForwards() { return Step.SOUTH; }
 		@Override Direction turnRight() { return WEST; }
 		@Override Direction turnLeft() { return EAST; }
 	},
 	WEST{
-		@Override Location moveBackwards() { return new Location(1,0); }
-		@Override Location moveForwards() { return new Location(- 1, 0); }
+		@Override Location moveBackwards() { return Step.EAST; }
+		@Override Location moveForwards() { return Step.WEST; }
 		@Override Direction turnRight() { return NORTH; }
 		@Override Direction turnLeft() { return SOUTH; }
 	};
---
+++ src/Step.java
@@ -0,0 +1,9 @@
+public class Step {
+	public static final Location SOUTH = new Location(0, - 1);
+
+	public static final Location NORTH = new Location(0,1);
+
+	public static final Location EAST = new Location(1, 0);
+
+	public static final Location WEST = new Location(-1, 0);
+}
